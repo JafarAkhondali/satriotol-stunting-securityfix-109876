04afd36d5666dbc0246b21e5b8a8d1809fffe5f0
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-10-17 10:39:11 +0700

    fixing beranda

--- cc-content/themes/cicool/views/header.php
+++ cc-content/themes/cicool/views/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Eduker - Online Course & Education HTML5 Template</title>
+    <title>STUNTING - KOTA SEMARANG</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
