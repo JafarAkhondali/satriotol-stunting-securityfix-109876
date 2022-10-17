cd41e2a46a78f3bd8b0d65c6e4bd047d134ab7b5
Akang Adnan <akangadnan@gmail.com>
2022-10-17 16:13:09 +0700

    Update error 404 page

--- cc-content/themes/cicool/views/error_404.php
+++ cc-content/themes/cicool/views/error_404.php
@@ -1,28 +1,85 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-<title>404 Page Not Found</title>
-<meta charset="utf-8">
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
-<meta name="author" content="">
+<!doctype html>
+<html class="no-js" lang="zxx">
 
-<title>Page Not Found</title>
-<link href="<?= theme_asset(); ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<style type="text/css">
-	.container {
-		margin-top: 10%;
-	}
-</style>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>ERROR 404 | Website Stunting</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Place favicon.ico in the root directory -->
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= base_url();?>assets_stunting/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url();?>assets_stunting/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url();?>assets_stunting/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?= base_url();?>assets_stunting/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?= base_url();?>assets_stunting/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+	<!-- CSS here -->
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/bootstrap.css">
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/meanmenu.css">
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/animate.css">
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/owl-carousel.css">
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/swiper-bundle.css">
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/backtotop.css">
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/magnific-popup.css">
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/nice-select.css">
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/font-awesome-pro.css">
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/spacing.css">
+	<link rel="stylesheet" href="<?= base_url();?>assets_stunting/css/style.css">
 </head>
+
 <body>
-    <div class="container text-center">
-    	<p><img src="<?= theme_asset(); ?>/img/404.png"></p>
-        <h2 class="text-muted">Page Not Found</h2>
-    </div>
+	<!--[if lte IE 9]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+      <![endif]-->
+
+	<!-- pre loader area start -->
+
+	<main>
+		<!-- error area start -->
+		<section class="error__area pt-120 pb-120">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-xxl-8">
+						<div class="error__content text-center">
+							<div class="error__thumb m-img mb-45">
+								<img src="<?= base_url();?>assets_stunting/img/error/error.png" alt="">
+							</div>
+							<div class="error__content">
+								<h3 class="error__title">Page Not Found</h3>
+								<p>Oops! The page you are looking for does not exist. It might have been moved or deleted.</p>
+								<div class="error__btn">
+									<a href="<?= base_url();?>" class="tp-btn">Back to home</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- error area end -->
+	</main>
+
+	<!-- JS here -->
+	<script src="<?= base_url();?>assets_stunting/js/vendor/jquery.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/vendor/waypoints.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/bootstrap-bundle.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/meanmenu.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/swiper-bundle.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/owl-carousel.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/magnific-popup.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/parallax.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/backtotop.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/nice-select.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/counterup.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/wow.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/isotope-pkgd.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/imagesloaded-pkgd.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/ajax-form.js"></script>
+	<script src="<?= base_url();?>assets_stunting/js/main.js"></script>
 </body>
+
 </html>
