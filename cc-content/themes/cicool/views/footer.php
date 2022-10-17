0c5315d67df2cea2fb8fca50bd1359e8f757019f
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-10-17 10:23:23 +0700

    layouting

--- cc-content/themes/cicool/views/footer.php
+++ cc-content/themes/cicool/views/footer.php
@@ -1,16 +1,156 @@
-    <!-- jQuery -->
-   
-    <!-- Bootstrap Core JavaScript -->
-    <script src="<?= theme_asset(); ?>/vendor/bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- Plugin JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    <script src="<?= theme_asset(); ?>/vendor/scrollreveal/scrollreveal.min.js"></script>
-    <script src="<?= theme_asset(); ?>/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+	<!-- footer area start -->
+	<footer>
+		<div class="footer__area">
+			<div class="footer__top grey-bg-4 pt-95 pb-45">
+				<div class="container">
+					<div class="row">
+						<div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-7">
+							<div class="footer__widget footer__widget-3 footer-col-3-1 mb-50">
+								<div class="footer__logo">
+									<div class="logo">
+										<a href="index.html">
+											<img src="assets_stunting/img/logo/logo-3.png" alt="">
+										</a>
+									</div>
+								</div>
+								<div class="footer__widget-content">
+									<div class="footer__widget-info">
+										<div class="footer__subscribe footer__subscribe-3">
+											<p>Educal University does not discriminate on the basis of race, nationa.
+											</p>
+											<form action="#">
+												<div class="footer__subscribe-input">
+													<input type="text" placeholder="Email">
+													<button type="submit" class="tp-btn-subscribe">Subscribe</button>
+												</div>
+											</form>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xxl-2 col-xl-2 col-lg-2 col-md-3 col-sm-5">
+							<div class="footer__widget footer__widget-3 footer-col-3-2 mb-50">
+								<h3 class="footer__widget-title footer__widget-title-3">Explore</h3>
+								<div class="footer__widget-content">
+									<ul>
+										<li>
+											<a href="#">About us</a>
+										</li>
+										<li>
+											<a href="#">Courses</a>
+										</li>
+										<li>
+											<a href="#">Educal University</a>
+										</li>
+										<li>
+											<a href="#">Instructor</a>
+										</li>
+										<li>
+											<a href="#">Events</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-xxl-3 col-xl-2 col-lg-2 col-md-3 col-sm-5">
+							<div class="footer__widget footer__widget-3 footer-col-3-3 mb-50">
+								<h3 class="footer__widget-title footer__widget-title-3">Links</h3>
+								<div class="footer__widget-content">
+									<ul>
+										<li>
+											<a href="#">Partners</a>
+										</li>
+										<li>
+											<a href="#">Support the canpaing</a>
+										</li>
+										<li>
+											<a href="#">News & Blogs</a>
+										</li>
+										<li>
+											<a href="#">Library</a>
+										</li>
+										<li>
+											<a href="#">Contact us</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-7">
+							<div class="footer__widget footer__widget-3 footer-col-3-4 mb-50">
+								<h3 class="footer__widget-title footer__widget-title-3">Information</h3>
 
-    <!-- Theme JavaScript -->
-    <script src="<?= theme_asset(); ?>/js/creative.min.js"></script>
+								<div class="footer__contact">
+									<ul>
+										<li>
+											<p>University Offices:</p>
+											<h4><a href="https://www.google.com/maps/place/%E0%A6%A2%E0%A6%BE%E0%A6%95%E0%A6%BE/@23.7807777,90.3492857,12z/data=!3m1!4b1!4m5!3m4!1s0x3755b8b087026b81:0x8fa563bbdd5904c2!8m2!3d23.810332!4d90.4125181"
+													target="_blank">382 NE 191st 87394 Miami,</a></h4>
+										</li>
+										<li>
+											<p>International:</p>
+											<h4><a href="tel:088-234-567-899">+(088) 234 567 899</a></h4>
+										</li>
+										<li>
+											<p>International:</p>
+											<h4><a href="mailto:info@educal.com">info@educal.com</a></h4>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer__bottom grey-bg-4">
+				<div class="container">
+					<div class="footer__bottom-inner">
+						<div class="row">
+							<div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+								<div class="footer__bottom-link">
+									<ul>
+										<li><a href="#">Disclaimer & Copyright</a></li>
+										<li><a href="#">Accessibility</a></li>
+										<li><a href="#">Privacy</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+								<div class="footer__social footer__social-3 text-md-end">
+									<ul>
+										<li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+										<li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+										<li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- footer area end -->
 
+	<!-- JS here -->
+	<script src="assets_stunting/js/vendor/jquery.js"></script>
+	<script src="assets_stunting/js/vendor/waypoints.js"></script>
+	<script src="assets_stunting/js/bootstrap-bundle.js"></script>
+	<script src="assets_stunting/js/meanmenu.js"></script>
+	<script src="assets_stunting/js/swiper-bundle.js"></script>
+	<script src="assets_stunting/js/owl-carousel.js"></script>
+	<script src="assets_stunting/js/magnific-popup.js"></script>
+	<script src="assets_stunting/js/parallax.js"></script>
+	<script src="assets_stunting/js/backtotop.js"></script>
+	<script src="assets_stunting/js/nice-select.js"></script>
+	<script src="assets_stunting/js/counterup.js"></script>
+	<script src="assets_stunting/js/wow.js"></script>
+	<script src="assets_stunting/js/isotope-pkgd.js"></script>
+	<script src="assets_stunting/js/imagesloaded-pkgd.js"></script>
+	<script src="assets_stunting/js/ajax-form.js"></script>
+	<script src="assets_stunting/js/main.js"></script>
 </body>
 
 </html>
--- cc-content/themes/cicool/views/header.php
+++ cc-content/themes/cicool/views/header.php
@@ -1,36 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html class="no-js" lang="zxx">
 
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Eduker - Online Course & Education HTML5 Template</title>
+    <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="<?= get_option('site_description'); ?>">
-    <meta name="keywords" content="<?= get_option('keywords'); ?>">
-    <meta name="author" content="<?= get_option('author'); ?>">
 
-    <title> <?= isset($title) ? $title : site_name() ?></title>
+    <!-- Place favicon.ico in the root directory -->
+    <!-- <link rel="shortcut icon" type="image/x-icon" href="assets_stunting/favicon/favicon.ico"> -->
 
-    <link href="<?= theme_asset(); ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets_stunting/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets_stunting/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets_stunting/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets_stunting/favicon/site.webmanifest">
+    <link rel="mask-icon" href="assets_stunting/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 
-    <link href="<?= theme_asset(); ?>/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets_stunting/css/bootstrap.css">
+    <link rel="stylesheet" href="assets_stunting/css/meanmenu.css">
+    <link rel="stylesheet" href="assets_stunting/css/animate.css">
+    <link rel="stylesheet" href="assets_stunting/css/owl-carousel.css">
+    <link rel="stylesheet" href="assets_stunting/css/swiper-bundle.css">
+    <link rel="stylesheet" href="assets_stunting/css/backtotop.css">
+    <link rel="stylesheet" href="assets_stunting/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets_stunting/css/nice-select.css">
+    <link rel="stylesheet" href="assets_stunting/css/font-awesome-pro.css">
+    <link rel="stylesheet" href="assets_stunting/css/spacing.css">
+    <link rel="stylesheet" href="assets_stunting/css/style.css">
 
-    <link href="<?= theme_asset(); ?>/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+    <style type="text/css">
+        .carousel-caption {
+            right: 0px;
+            bottom: 0px;
+            left: 0px;
+            background: RGB(0, 0, 0, 0.3);
+        }
 
-    <link href="<?= theme_asset(); ?>/css/creative.css" rel="stylesheet">
-    
-    <link rel="stylesheet" href="<?= BASE_ASSET; ?>admin-lte/plugins/morris/morris.css">
-    <link rel="stylesheet" href="<?= BASE_ASSET; ?>flag-icon/css/flag-icon.css" rel="stylesheet" media="all" />
+        .carousel-caption h2,
+        .carousel-caption .carousel-caption-description {
+            color: white;
+        }
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    
-    <script src="<?= theme_asset(); ?>/vendor/jquery/jquery.min.js"></script>
-
+        .carousel-caption .carousel-caption-description p {
+            color: white;
+        }
+    </style>
 </head>
