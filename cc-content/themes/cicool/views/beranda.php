13cd3d122da5a2511c31739a93018629a7f08ce6
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-10-18 09:00:25 +0700

    fixing beranda

--- cc-content/themes/cicool/views/beranda.php
+++ cc-content/themes/cicool/views/beranda.php
@@ -348,89 +348,6 @@
         </div>
     </section>
     <!-- course area end -->
-
-    <!-- price area start -->
-    <section class="price__area pb-85">
-        <div class="container">
-            <div class="row">
-                <div class="col-xxl-12">
-                    <div class="section__title-wrapper text-center mb-60">
-                        <span class="section__title-pre-3">Our Pricing</span>
-                        <h2 class="section__title-2">Simple, transparent pricing</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
-                    <div class="price__banner theme-bg-3 mb-30 fix p-relative">
-                        <div class="price__shape">
-                            <img src="<?= base_url(); ?>assets_stunting/img/price/price-shape.png" alt="">
-                        </div>
-                        <div class="price__banner-content p-relative z-index-1">
-                            <h3>
-                                <span>Save More</span>
-                                With Goodplans.
-                            </h3>
-                            <p>Choose a plan and getunboard minutes.Then get $100 credits for your next payment</p>
-
-                            <a href="contact.html" class="tp-price-btn">Choose Plan</a>
-                        </div>
-                        <div class="price__thumb">
-                            <img src="<?= base_url(); ?>assets_stunting/img/price/price-thumb.png" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
-                    <div class="price__item white-bg mb-30 transition-3 fix">
-                        <h3 class="price__title">Day pass</h3>
-
-                        <div class="price__content">
-                            <div class="price__list mb-35">
-                                <ul>
-                                    <li>All limited links <span><i class="fa-solid fa-check"></i></span></li>
-                                    <li>Own analytics platform <span><i class="fa-solid fa-check"></i></span></li>
-                                    <li>Chat support <span><i class="fa-solid fa-check"></i></span></li>
-                                    <li class="unavailable">Optimuze hashtags <span><i class="fa-solid fa-xmark"></i></span></li>
-                                    <li class="unavailable">Unlimited user <span><i class="fa-solid fa-xmark"></i></span></li>
-                                </ul>
-                            </div>
-                            <div class="price__amount mb-30">
-                                <h4>$16.00 <span>/ perday</span></h4>
-                            </div>
-                            <div class="price__btn">
-                                <a href="contact.html" class="tp-btn-9 tp-btn-12 w-100">Choose Plan</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
-                    <div class="price__item active white-bg mb-30 transition-3 fix">
-                        <h3 class="price__title">Monthly pass</h3>
-
-                        <div class="price__content">
-                            <div class="price__list mb-35">
-                                <ul>
-                                    <li>All limited links <span><i class="fa-solid fa-check"></i></span></li>
-                                    <li>Own analytics platform <span><i class="fa-solid fa-check"></i></span></li>
-                                    <li>Chat support <span><i class="fa-solid fa-check"></i></span></li>
-                                    <li>Optimuze hashtags <span><i class="fa-solid fa-xmark"></i></span></li>
-                                    <li>Unlimited user <span><i class="fa-solid fa-xmark"></i></span></li>
-                                </ul>
-                            </div>
-                            <div class="price__amount mb-30">
-                                <h4>$34.00 <span>/ monthly</span></h4>
-                            </div>
-                            <div class="price__btn">
-                                <a href="contact.html" class="tp-btn-9 tp-btn-12 w-100">Choose Plan</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- price area end -->
-
     <!-- faq area start -->
     <section class="faq__area pt-20 pb-130">
         <div class="container">
@@ -443,10 +360,6 @@
                         </div>
                         <p>Join our club member community now to get free updates and also a lot of freebies are
                             waiting for you or <a href="contact.html">Contact Us</a></p>
-
-                        <div class="faq__btn">
-                            <a href="about.html" class="tp-btn-5 tp-btn-13">Read our full FAQ</a>
-                        </div>
                     </div>
                 </div>
                 <div class="col-xxl-7 col-xl-7 col-lg-7">
