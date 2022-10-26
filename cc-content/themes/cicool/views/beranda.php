5e612e5ac49b67af74570f71215b6fe5bb242265
Akang Adnan <akangadnan@gmail.com>
2022-10-26 07:57:17 +0700

    Perubahan Beranda

--- cc-content/themes/cicool/views/beranda.php
+++ cc-content/themes/cicool/views/beranda.php
@@ -172,7 +172,91 @@
 			<div class="row">
 				<?php foreach ($blogs as $blog) { ?>
 				<div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6">
-					<div class="course__item white-bg transition-3 mb-30">
+					<div class="course__item-2 transition-3 white-bg mb-30 fix">
+						<div class="course__thumb-2 w-img fix">
+							<a href="javascript:void(0);">
+					<?php
+						if (!empty($blog->image)) {
+							$file = FCPATH.'uploads/blog/'.$blog->image;
+
+							if (is_image($blog->image)) {
+								if (file_exists($file)) {
+									echo '<img src="'.base_url().'uploads/blog/'.$blog->image.'" alt="">';
+								}else{
+									$title = str_replace(' ', '+', $blog->title);
+	
+									echo '<img src="https://via.placeholder.com/760x405.png?text='.$title.'" alt="">';
+								}
+							} else{
+								$title = str_replace(' ', '+', $blog->title);
+
+								echo '<img src="https://via.placeholder.com/760x405.png?text='.$title.'" alt="">';
+							}
+						}else{
+							$title = str_replace(' ', '+', $blog->title);
+
+							echo '<img src="https://via.placeholder.com/760x405.png?text='.$title.'" alt="">';
+						}
+					?>
+							</a>
+						</div>
+						<div class="course__content-2">
+							<div class="course__top-2 d-flex align-items-center justify-content-between">
+								<div class="course__tag-2">
+									<a href="<?= $blog->kategori_id;?>"><?= $blog->nama_kategori;?></a>
+								</div>
+							</div>
+							<h3 class="course__title-2">
+								<a href="javascript:void(0);">
+						<?php
+							if (strlen($blog->title) < 50) {
+								echo $blog->title;
+							}else{
+								echo substr($blog->title, 0, 50).'...';
+							}
+						?>
+								</a>
+							</h3>
+							<div class="course__bottom-2 d-flex align-items-center justify-content-between">
+								<div class="course__action">
+									<ul>
+										<li>
+											<div class="course__action-item d-flex align-items-center">
+												<div class="course__action-icon mr-5">
+													<span><svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+														<path d="M5.00004 5.5833C6.28592 5.5833 7.32833 4.5573 7.32833 3.29165C7.32833 2.02601 6.28592 1 5.00004 1C3.71416 1 2.67175 2.02601 2.67175 3.29165C2.67175 4.5573 3.71416 5.5833 5.00004 5.5833Z" stroke="#5F6160" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+														<path d="M9 11.0001C9 9.22632 7.20722 7.79175 5 7.79175C2.79278 7.79175 1 9.22632 1 11.0001" stroke="#5F6160" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+													</svg></span>
+												</div>
+												<div class="course__action-content"><span><?= $blog->user_username;?></span></div>
+											</div>
+										</li>
+										<li>
+											<div class="course__action-item d-flex align-items-center">
+												<div class="course__action-icon mr-5">
+													<span><svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+														<path d="M9.01232 5.95416C9.01232 7.06709 8.11298 7.96642 7.00006 7.96642C5.88713 7.96642 4.98779 7.06709 4.98779 5.95416C4.98779 4.84123 5.88713 3.94189 7.00006 3.94189C8.11298 3.94189 9.01232 4.84123 9.01232 5.95416Z" stroke="#5F6160" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
+														<path d="M7 10.6026C8.98416 10.6026 10.8334 9.43342 12.1206 7.40991C12.6265 6.61737 12.6265 5.28523 12.1206 4.49269C10.8334 2.46919 8.98416 1.30005 7 1.30005C5.01584 1.30005 3.16658 2.46919 1.87941 4.49269C1.37353 5.28523 1.37353 6.61737 1.87941 7.40991C3.16658 9.43342 5.01584 10.6026 7 10.6026Z" stroke="#5F6160" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
+													</svg></span>
+												</div>
+												<div class="course__action-content">
+													<span><?= $blog->viewers;?></span>
+												</div>
+											</div>
+										</li>
+									</ul>
+								</div>
+								<div class="course__tutor-2">
+									<a href="#">
+										<img src="<?= base_url();?>assets_stunting/img/course/tutor/course-tutor-1.jpg" alt="">
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+
+					<!-- <div class="course__item white-bg transition-3 mb-30">
 						<div class="course__thumb w-img fix">
 							<a href="course-details.html">
 								<img src="<?php echo base_url();?>uploads/blog/<?= $blog->image;?>" alt="">
@@ -206,13 +290,19 @@
 						?>
 							</div>
 						</div>
-					</div>
+					</div> -->
 				</div>
 				<?php } ?>
 			</div>
 		</div>
 	</section>
 	<!-- course area start -->
+
+
+	<!-- Kebijakan get Started in here -->
+
+
+
 	<!-- faq area start -->
 	<section class="faq__area pt-20 pb-130">
 		<div class="container">
