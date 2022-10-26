08d6d12ef440281bd5c4b74b81655e23bafca426
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-10-26 09:11:15 +0700

    add category

--- cc-content/themes/cicool/views/beranda.php
+++ cc-content/themes/cicool/views/beranda.php
@@ -1,23 +1,20 @@
 <?= get_header(); ?>
-
 <main>
 	<!-- slider area start -->
 	<section class="slider__area">
 		<div class="slider__active swiper-container">
 			<div class="swiper-wrapper">
 				<?php foreach ($sliders as $slider) { ?>
-				<div
-					class="slider__item swiper-slide p-relative slider__height slider__height-3 d-flex align-items-center z-index-1">
-					<div class="slider__bg slider__overlay slider__overlay-3 include-bg"
-						data-background="<?php echo base_url(); ?>uploads/sliders/<?php echo $slider->slider_image; ?>">
-					</div>
-					<div class="carousel-caption">
-						<h2><?php echo $slider->slider_title; ?></h2>
-						<div class="carousel-caption-description">
-							<p><?php echo $slider->slider_subtitle; ?></p>
+					<div class="slider__item swiper-slide p-relative slider__height slider__height-3 d-flex align-items-center z-index-1">
+						<div class="slider__bg slider__overlay slider__overlay-3 include-bg" data-background="<?php echo base_url(); ?>uploads/sliders/<?php echo $slider->slider_image; ?>">
+						</div>
+						<div class="carousel-caption">
+							<h2><?php echo $slider->slider_title; ?></h2>
+							<div class="carousel-caption-description">
+								<p><?php echo $slider->slider_subtitle; ?></p>
+							</div>
 						</div>
 					</div>
-				</div>
 				<?php } ?>
 			</div>
 			<div class="main-slider-paginations main-slider-paginations-2">
@@ -32,7 +29,7 @@
 		<div class="container">
 			<div class="certificate__inner grey-bg-9 p-relative">
 				<div class="certificate__thumb">
-					<img src="<?= base_url();?>assets_stunting/img/certificate/certificate.png" alt="">
+					<img src="<?= base_url(); ?>assets_stunting/img/certificate/certificate.png" alt="">
 				</div>
 				<div class="row">
 					<div class="col-xxl-6">
@@ -41,7 +38,7 @@
 								<span class="section__title-pre-3">Stunting Kota Semarang</span>
 								<h2 class="section__title section__title-44">Kata Pengantar</h2>
 							</div>
-							<p><?php echo $about->about_description;?></p>
+							<p><?php echo $about->about_description; ?></p>
 							<div class="certificate__links d-sm-flex align-items-center">
 							</div>
 						</div>
@@ -53,9 +50,9 @@
 
 	<section class="research__area research__border grey-bg-3 pt-115 pb-90 p-relative z-index-1">
 		<div class="research__shape">
-			<img class="research__shape-1 d-none d-sm-block" src="<?php echo base_url();?>assets_stunting/img/research/research-shape-1.png" alt="">
-			<img class="research__shape-2 d-none d-sm-block" src="<?php echo base_url();?>assets_stunting/img/research/research-shape-2.png" alt="">
-			<img class="research__shape-3" src="<?php echo base_url();?>assets_stunting/img/research/research-shape-3.png" alt="">
+			<img class="research__shape-1 d-none d-sm-block" src="<?php echo base_url(); ?>assets_stunting/img/research/research-shape-1.png" alt="">
+			<img class="research__shape-2 d-none d-sm-block" src="<?php echo base_url(); ?>assets_stunting/img/research/research-shape-2.png" alt="">
+			<img class="research__shape-3" src="<?php echo base_url(); ?>assets_stunting/img/research/research-shape-3.png" alt="">
 		</div>
 		<div class="container">
 			<div class="row">
@@ -112,18 +109,18 @@
 						<div class="section__title-wrapper mb-15">
 							<h2 class="section__title">Pengertian Stunting</h2>
 						</div>
-						<p><?php echo $about->about_pengertian;?></p>
+						<p><?php echo $about->about_pengertian; ?></p>
 					</div>
 				</div>
 				<div class="col-xxl-6 col-xl-6 col-lg-6">
 					<div class="about__thumb-wrapper d-sm-flex mr-20 p-relative">
 						<div class="about__shape">
-							<img class="about__shape-1 d-none d-sm-block" src="<?php echo base_url();?>assets_stunting/img/about/about-shape-1.png" alt="">
-							<img class="about__shape-2 d-none d-sm-block" src="<?php echo base_url();?>assets_stunting/img/about/about-shape-2.png" alt="">
-							<img class="about__shape-3" src="<?php echo base_url();?>assets_stunting/img/about/about-shape-3.png" alt="">
+							<img class="about__shape-1 d-none d-sm-block" src="<?php echo base_url(); ?>assets_stunting/img/about/about-shape-1.png" alt="">
+							<img class="about__shape-2 d-none d-sm-block" src="<?php echo base_url(); ?>assets_stunting/img/about/about-shape-2.png" alt="">
+							<img class="about__shape-3" src="<?php echo base_url(); ?>assets_stunting/img/about/about-shape-3.png" alt="">
 						</div>
 						<div class="about__thumb-2 mb-10">
-							<img src="<?php echo base_url();?>assets_stunting/stunting/pengertian.jpg" alt="Pengertian Stunting" style="width: 580px; border-radius: 20px;">
+							<img src="<?php echo base_url(); ?>assets_stunting/stunting/pengertian.jpg" alt="Pengertian Stunting" style="width: 580px; border-radius: 20px;">
 						</div>
 					</div>
 				</div>
@@ -136,12 +133,12 @@
 				<div class="col-xxl-6 col-xl-6 col-lg-6">
 					<div class="about__thumb-wrapper d-sm-flex mr-20 p-relative">
 						<div class="about__shape">
-							<img class="about__shape-1 d-none d-sm-block" src="<?php echo base_url();?>assets_stunting/img/about/about-shape-1.png" alt="">
-							<img class="about__shape-2 d-none d-sm-block" src="<?php echo base_url();?>assets_stunting/img/about/about-shape-2.png" alt="">
-							<img class="about__shape-3" src="<?php echo base_url();?>assets_stunting/img/about/about-shape-3.png" alt="">
+							<img class="about__shape-1 d-none d-sm-block" src="<?php echo base_url(); ?>assets_stunting/img/about/about-shape-1.png" alt="">
+							<img class="about__shape-2 d-none d-sm-block" src="<?php echo base_url(); ?>assets_stunting/img/about/about-shape-2.png" alt="">
+							<img class="about__shape-3" src="<?php echo base_url(); ?>assets_stunting/img/about/about-shape-3.png" alt="">
 						</div>
 						<div class="about__thumb-2 mb-10">
-							<img src="<?php echo base_url();?>assets_stunting/stunting/penyebab.jpg" alt="Penyebab Stunting" style="width: 580px; border-radius: 20px;">
+							<img src="<?php echo base_url(); ?>assets_stunting/stunting/penyebab.jpg" alt="Penyebab Stunting" style="width: 580px; border-radius: 20px;">
 						</div>
 					</div>
 				</div>
@@ -151,8 +148,8 @@
 							<h2 class="section__title">Penyebab Stunting</h2>
 						</div>
 						<div class="about__list mb-40">
-							<p><?php echo $about->about_penyebab;?></p>
-                        </div>
+							<p><?php echo $about->about_penyebab; ?></p>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -165,133 +162,147 @@
 			<div class="row">
 				<div class="col-xxl-12">
 					<div class="section__title-wrapper text-center mb-60">
-						<h2 class="section__title section__title-44">Berita Terbaru</h2>
+						<h2 class="section__title section__title-44">Program Kegiatan</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xxl-12">
+					<div class="course__filter text-center mb-30">
+						<nav>
+							<div class="nav nav-tabs justify-content-center" id="course-tab" role="tablist">
+								<?php foreach ($categories as $category) { ?>
+									<button class="nav-link" id="nav-trending-tab" data-bs-toggle="tab" data-bs-target="#nav-trending" type="button" role="tab" aria-controls="nav-trending" aria-selected="false"><?= $category->category_name; ?></button>
+								<?php } ?>
+
+							</div>
+						</nav>
 					</div>
 				</div>
 			</div>
 			<div class="row">
 				<?php foreach ($blogs as $blog) { ?>
-				<div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6">
-					<div class="course__item-2 transition-3 white-bg mb-30 fix">
-						<div class="course__thumb-2 w-img fix">
-							<a href="<?= base_url().'blog/'.$blog->slug;?>">
-					<?php
-						if (!empty($blog->image)) {
-							$file = FCPATH.'uploads/blog/'.$blog->image;
+					<div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6">
+						<div class="course__item-2 transition-3 white-bg mb-30 fix">
+							<div class="course__thumb-2 w-img fix">
+								<a href="<?= base_url() . 'blog/' . $blog->slug; ?>">
+									<?php
+									if (!empty($blog->image)) {
+										$file = FCPATH . 'uploads/blog/' . $blog->image;
 
-							if (is_image($blog->image)) {
-								if (file_exists($file)) {
-									echo '<img src="'.base_url().'uploads/blog/'.$blog->image.'" alt="">';
-								}else{
-									$title = str_replace(' ', '+', $blog->title);
-	
-									echo '<img src="https://via.placeholder.com/760x405.png?text='.$title.'" alt="">';
-								}
-							} else{
-								$title = str_replace(' ', '+', $blog->title);
+										if (is_image($blog->image)) {
+											if (file_exists($file)) {
+												echo '<img src="' . base_url() . 'uploads/blog/' . $blog->image . '" alt="">';
+											} else {
+												$title = str_replace(' ', '+', $blog->title);
 
-								echo '<img src="https://via.placeholder.com/760x405.png?text='.$title.'" alt="">';
-							}
-						}else{
-							$title = str_replace(' ', '+', $blog->title);
+												echo '<img src="https://via.placeholder.com/760x405.png?text=' . $title . '" alt="">';
+											}
+										} else {
+											$title = str_replace(' ', '+', $blog->title);
 
-							echo '<img src="https://via.placeholder.com/760x405.png?text='.$title.'" alt="">';
-						}
-					?>
-							</a>
-						</div>
-						<div class="course__content-2">
-							<div class="course__top-2 d-flex align-items-center justify-content-between">
-								<div class="course__tag-2">
-									<a href="<?= $blog->kategori_id;?>"><?= $blog->nama_kategori;?></a>
-								</div>
-							</div>
-							<h3 class="course__title-2">
-							<a href="<?= base_url().'blog/'.$blog->slug;?>">
-						<?php
-							if (strlen($blog->title) < 50) {
-								echo $blog->title;
-							}else{
-								echo substr($blog->title, 0, 50).'...';
-							}
-						?>
+											echo '<img src="https://via.placeholder.com/760x405.png?text=' . $title . '" alt="">';
+										}
+									} else {
+										$title = str_replace(' ', '+', $blog->title);
+
+										echo '<img src="https://via.placeholder.com/760x405.png?text=' . $title . '" alt="">';
+									}
+									?>
 								</a>
-							</h3>
-							<div class="course__bottom-2 d-flex align-items-center justify-content-between">
-								<div class="course__action">
-									<ul>
-										<li>
-											<div class="course__action-item d-flex align-items-center">
-												<div class="course__action-icon mr-5">
-													<span><svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-														<path d="M5.00004 5.5833C6.28592 5.5833 7.32833 4.5573 7.32833 3.29165C7.32833 2.02601 6.28592 1 5.00004 1C3.71416 1 2.67175 2.02601 2.67175 3.29165C2.67175 4.5573 3.71416 5.5833 5.00004 5.5833Z" stroke="#5F6160" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-														<path d="M9 11.0001C9 9.22632 7.20722 7.79175 5 7.79175C2.79278 7.79175 1 9.22632 1 11.0001" stroke="#5F6160" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-													</svg></span>
-												</div>
-												<div class="course__action-content"><span><?= $blog->user_username;?></span></div>
-											</div>
-										</li>
-										<li>
-											<div class="course__action-item d-flex align-items-center">
-												<div class="course__action-icon mr-5">
-													<span><svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-														<path d="M9.01232 5.95416C9.01232 7.06709 8.11298 7.96642 7.00006 7.96642C5.88713 7.96642 4.98779 7.06709 4.98779 5.95416C4.98779 4.84123 5.88713 3.94189 7.00006 3.94189C8.11298 3.94189 9.01232 4.84123 9.01232 5.95416Z" stroke="#5F6160" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
-														<path d="M7 10.6026C8.98416 10.6026 10.8334 9.43342 12.1206 7.40991C12.6265 6.61737 12.6265 5.28523 12.1206 4.49269C10.8334 2.46919 8.98416 1.30005 7 1.30005C5.01584 1.30005 3.16658 2.46919 1.87941 4.49269C1.37353 5.28523 1.37353 6.61737 1.87941 7.40991C3.16658 9.43342 5.01584 10.6026 7 10.6026Z" stroke="#5F6160" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
-													</svg></span>
-												</div>
-												<div class="course__action-content">
-													<span><?= $blog->viewers;?></span>
-												</div>
-											</div>
-										</li>
-									</ul>
+							</div>
+							<div class="course__content-2">
+								<div class="course__top-2 d-flex align-items-center justify-content-between">
+									<div class="course__tag-2">
+										<a href="<?= $blog->kategori_id; ?>"><?= $blog->nama_kategori; ?></a>
+									</div>
 								</div>
-								<div class="course__tutor-2">
-									<a href="#">
-										<img src="<?= base_url();?>assets_stunting/img/course/tutor/course-tutor-1.jpg" alt="">
+								<h3 class="course__title-2">
+									<a href="<?= base_url() . 'blog/' . $blog->slug; ?>">
+										<?php
+										if (strlen($blog->title) < 50) {
+											echo $blog->title;
+										} else {
+											echo substr($blog->title, 0, 50) . '...';
+										}
+										?>
 									</a>
+								</h3>
+								<div class="course__bottom-2 d-flex align-items-center justify-content-between">
+									<div class="course__action">
+										<ul>
+											<li>
+												<div class="course__action-item d-flex align-items-center">
+													<div class="course__action-icon mr-5">
+														<span><svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+																<path d="M5.00004 5.5833C6.28592 5.5833 7.32833 4.5573 7.32833 3.29165C7.32833 2.02601 6.28592 1 5.00004 1C3.71416 1 2.67175 2.02601 2.67175 3.29165C2.67175 4.5573 3.71416 5.5833 5.00004 5.5833Z" stroke="#5F6160" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+																<path d="M9 11.0001C9 9.22632 7.20722 7.79175 5 7.79175C2.79278 7.79175 1 9.22632 1 11.0001" stroke="#5F6160" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+															</svg></span>
+													</div>
+													<div class="course__action-content"><span><?= $blog->user_username; ?></span></div>
+												</div>
+											</li>
+											<li>
+												<div class="course__action-item d-flex align-items-center">
+													<div class="course__action-icon mr-5">
+														<span><svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+																<path d="M9.01232 5.95416C9.01232 7.06709 8.11298 7.96642 7.00006 7.96642C5.88713 7.96642 4.98779 7.06709 4.98779 5.95416C4.98779 4.84123 5.88713 3.94189 7.00006 3.94189C8.11298 3.94189 9.01232 4.84123 9.01232 5.95416Z" stroke="#5F6160" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
+																<path d="M7 10.6026C8.98416 10.6026 10.8334 9.43342 12.1206 7.40991C12.6265 6.61737 12.6265 5.28523 12.1206 4.49269C10.8334 2.46919 8.98416 1.30005 7 1.30005C5.01584 1.30005 3.16658 2.46919 1.87941 4.49269C1.37353 5.28523 1.37353 6.61737 1.87941 7.40991C3.16658 9.43342 5.01584 10.6026 7 10.6026Z" stroke="#5F6160" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
+															</svg></span>
+													</div>
+													<div class="course__action-content">
+														<span><?= $blog->viewers; ?></span>
+													</div>
+												</div>
+											</li>
+										</ul>
+									</div>
+									<div class="course__tutor-2">
+										<a href="#">
+											<img src="<?= base_url(); ?>assets_stunting/img/course/tutor/course-tutor-1.jpg" alt="">
+										</a>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
 
 
-					<!-- <div class="course__item white-bg transition-3 mb-30">
+						<!-- <div class="course__item white-bg transition-3 mb-30">
 						<div class="course__thumb w-img fix">
 							<a href="course-details.html">
-								<img src="<?php echo base_url();?>uploads/blog/<?= $blog->image;?>" alt="">
+								<img src="<?php echo base_url(); ?>uploads/blog/<?= $blog->image; ?>" alt="">
 							</a>
 						</div>
 						<div class="course__content p-relative">
 							<div class="course__tag">
-								<a href="#"><?= $blog->nama_kategori;?></a>
+								<a href="#"><?= $blog->nama_kategori; ?></a>
 							</div>
 							<h3 class="course__title">
 								<a href="course-details.html"><?php echo $blog->title; ?></a>
 							</h3>
 							<p>
 						<?php
-							if (strlen($blog->content) < 120) {
-								echo strip_tags($blog->content);
-							}else{
-								echo strip_tags(substr($blog->content, 0, 120)).'...';
-							}
+						if (strlen($blog->content) < 120) {
+							echo strip_tags($blog->content);
+						} else {
+							echo strip_tags(substr($blog->content, 0, 120)) . '...';
+						}
 						?>
 							</p>
 
 							<div class="course__bottom d-sm-flex align-items-center justify-content-between">
 								<div class="course__tutor">
-									<a href="javascript:void(0);"><i class="fa fa-user"></i> <?= ucwords(strtolower($blog->user_username));?></a>
+									<a href="javascript:void(0);"><i class="fa fa-user"></i> <?= ucwords(strtolower($blog->user_username)); ?></a>
 								</div>
 						<?php
-							if (strlen($blog->content) > 120) {
-								echo '<div class="course__lesson"><a href="javascript:void(0);">Read more</a></div>';
-							}
+						if (strlen($blog->content) > 120) {
+							echo '<div class="course__lesson"><a href="javascript:void(0);">Read more</a></div>';
+						}
 						?>
 							</div>
 						</div>
 					</div> -->
-				</div>
+					</div>
 				<?php } ?>
 			</div>
 		</div>
@@ -321,27 +332,27 @@
 					<div class="faq__item-wrapper pl-100">
 						<div class="faq__accordion">
 							<div class="accordion" id="faqaccordion">
-						<?php
-							$no = '1';
-							foreach ($faqs as $faq) {
-						?>
-							<div class="accordion-item">
-								<h2 class="accordion-header" id="faq<?= $no;?>">
-									<button class="accordion-button <?= $no != '1' ?  'collapsed' : '';?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $no;?>" aria-expanded="true" aria-controls="collapse<?= $no;?>">
-										<?= $faq->faq_question;?>
-									</button>
-								</h2>
-								<div id="collapse<?= $no;?>" class="accordion-collapse collapse <?= $no == '1' ?  'show' : '';?>" aria-labelledby="faq<?= $no;?>" data-bs-parent="#faqaccordion">
-									<div class="accordion-body">
-										<p><?= $faq->faq_answer;?></p>
+								<?php
+								$no = '1';
+								foreach ($faqs as $faq) {
+								?>
+									<div class="accordion-item">
+										<h2 class="accordion-header" id="faq<?= $no; ?>">
+											<button class="accordion-button <?= $no != '1' ?  'collapsed' : ''; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $no; ?>" aria-expanded="true" aria-controls="collapse<?= $no; ?>">
+												<?= $faq->faq_question; ?>
+											</button>
+										</h2>
+										<div id="collapse<?= $no; ?>" class="accordion-collapse collapse <?= $no == '1' ?  'show' : ''; ?>" aria-labelledby="faq<?= $no; ?>" data-bs-parent="#faqaccordion">
+											<div class="accordion-body">
+												<p><?= $faq->faq_answer; ?></p>
+											</div>
+										</div>
 									</div>
-								</div>
-							</div>
-						<?php
-								$no++;
-							}
-						?>
-								
+								<?php
+									$no++;
+								}
+								?>
+
 							</div>
 						</div>
 					</div>
