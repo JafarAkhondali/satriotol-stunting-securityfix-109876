b7dcaadae7e58ac5f85bcbb421a69bceb7f2ab6b
Akang Adnan <akangadnan@gmail.com>
2022-11-07 07:52:44 +0700

    Lokus Stunting

--- cc-content/themes/cicool/views/file-lokus-stunting.php
+++ cc-content/themes/cicool/views/file-lokus-stunting.php
@@ -28,23 +28,16 @@
 <?php
 	}else{
 ?>
-		<section class="error__area">
-			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-xxl-8">
-						<div class="error__content text-center">
-							<div class="error__thumb m-img">
-								<img src="<?= base_url();?>assets_stunting/img/error/file-not-found.webp" alt="">
-							</div>
-							<div class="error__content">
-								<h3 class="error__title">Data Not Available</h3>
-								<p>Oops! The data you are looking for does not available.</p>
-							</div>
-						</div>
-					</div>
+		
+			<div class="error__content text-center">
+				<div class="error__thumb m-img">
+					<img src="<?= base_url();?>assets_stunting/img/error/file-not-found.webp" alt="">
+				</div>
+				<div class="error__content">
+					<h3 class="error__title">Data Not Available</h3>
+					<p>Oops! The data you are looking for does not available.</p>
 				</div>
 			</div>
-		</section>
 <?php
 	}
 ?>
