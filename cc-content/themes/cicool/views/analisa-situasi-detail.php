e5eb72f19eb4900ba3665be8b1d280133cadc5f3
Akang Adnan <akangadnan@gmail.com>
2022-11-12 21:47:39 +0700

    Analisa SItuasi + Chart

--- cc-content/themes/cicool/views/analisa-situasi-detail.php
+++ cc-content/themes/cicool/views/analisa-situasi-detail.php
@@ -1,4 +1,26 @@
 <?= get_header(); ?>
+
+<?php
+    $indikators = [];
+    $cakupans   = [];
+    $colors     = [];
+
+    foreach ($details as $item) {
+        $indikators[]   = $item->analisa_situasi_aksi_indikator;
+        $cakupans[]     = $item->analisa_situasi_aksi_cakupan;
+        $colors[]       = $item->analisa_situasi_aksi_warna;
+    }
+
+    $data = [
+        $indikators,
+        $cakupans,
+        $colors,
+    ];
+
+    // echo json_encode($data);
+    // exit();
+?>
+
 <main>
     <!-- event area start -->
     <section class="event__area pt-115 pb-115">
@@ -6,7 +28,7 @@
             <div class="row">
                 <div class="col-xxl-12">
                     <div class="section__title-wrapper-2 text-center mb-60">
-                        <h3 class="section__title-2">Analisa Stunting <?php echo $analisa->analisa_situasi_year;?></h3>
+                        <h3 class="section__title-2">Analisa Situasi <?php echo $analisa->analisa_situasi_year;?></h3>
                     </div>
                     <div class="row">
                         <div class="col-md-5">
@@ -15,28 +37,7 @@
                             </a>
                         </div>
                         <div class="col-md-7">
-                            <div class="table-content table-responsive">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>Indikator</th>
-                                            <th>Cakupan</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                <?php
-                                    foreach ($details as $detail) {
-                                ?>
-                                        <tr>
-                                            <td><?php echo $detail->analisa_situasi_aksi_indikator;?></td>
-                                            <td><?php echo $detail->analisa_situasi_aksi_cakupan;?></td>
-                                        </tr>
-                                <?php
-                                    }
-                                ?>
-                                    </tbody>
-                                </table>
-                            </div>
+                            <canvas id="myChart" width="400" height="400"></canvas>
                         </div>
                     </div>
                 </div>
@@ -44,4 +45,40 @@
         </div>
     </section>
 </main>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script type="text/javascript">
+    const ctx           = document.getElementById('myChart').getContext('2d');
+
+    const data = {
+        labels: <?php echo json_encode($indikators);?>,
+        datasets: [{
+            label: 'Analisa Situasi '+<?php echo $analisa->analisa_situasi_year;?>,
+            data: <?php echo json_encode($cakupans);?>,
+            backgroundColor: <?php echo json_encode($colors);?>,
+            hoverOffset: 4,
+            borderColor: '#a4a4a4',
+            borderWidth: 1,
+        }]
+    };
+
+    const myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Analisa Situasi '+<?php echo $analisa->analisa_situasi_year;?>,
+            }
+            }
+        },
+    });
+</script>
+
 <?= get_footer(); ?>
