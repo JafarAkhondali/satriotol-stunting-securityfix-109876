a153d1b690fbb44de228b1851db06e93b13ac2c3
Akang Adnan <akangadnan@gmail.com>
2022-11-15 13:20:36 +0700

    add assets + controller Color Picker

--- cc-content/themes/cicool/views/rentan-opd.php
+++ cc-content/themes/cicool/views/rentan-opd.php
@@ -26,6 +26,9 @@
             </div>
             <div class="row">
 				<div class="col-md-12">
+			<?php
+				if (count($opds) > 0) {
+			?>
 					<div class="table-content table-responsive">
 						<table class="table">
 							<thead>
@@ -56,6 +59,22 @@
 							</tbody>
 						</table>
 					</div>
+			<?php
+				}else{
+			?>
+					
+						<div class="error__content text-center">
+							<div class="error__thumb m-img">
+								<img src="<?= base_url();?>assets_stunting/img/error/file-not-found.webp" alt="">
+							</div>
+							<div class="error__content">
+								<h3 class="error__title">Data Not Available</h3>
+								<p>Oops! The data you are looking for does not available.</p>
+							</div>
+						</div>
+			<?php
+				}
+			?>
 				</div>
 			</div>
         </div>
