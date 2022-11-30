994de5203cb0153c908b867eba0040c885e1a94f
Akang Adnan <akangadnan@gmail.com>
2022-11-30 12:18:54 +0700

    User -> OPD

--- modules/user/views/backend/standart/administrator/user/user_profile.php
+++ modules/user/views/backend/standart/administrator/user/user_profile.php
@@ -75,6 +75,7 @@
 							</div>
 							<div class="box-footer no-padding">
 								<ul class="nav nav-stacked">
+									<li><a href="javascript:void(0);"><i class="fa fa-chevron-right"></i> <?= $user->opd_nama; ?></a></li>
 									<?php foreach($this->aauth->get_user_groups() as $row): ?>
 									<li><a href="#"><i class="fa fa-chevron-right"></i> <?= _ent($row->name); ?></a>
 									</li>
