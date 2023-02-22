2bb7661635bc31ad42192ede85b60e0f1b5cd7f6
Akang Adnan <akangadnan@gmail.com>
2023-02-22 08:21:50 +0700

    pembaruan width Chart DTKS

--- cc-content/themes/cicool/views/chart-dtks.php
+++ cc-content/themes/cicool/views/chart-dtks.php
@@ -10,10 +10,10 @@
 						<div class="card-body">
 							<h5 class="card-title">Grafik Data Terpadu Kesejahteraan Sosial (DTKS)</h5>
 							<ul class="nav nav-tabs" id="myTab">
-								<li class="nav-item"> <a href="<?= base_url(); ?>chart-dtks-jenkel" target="_blank" class="nav-link active">berdasarkan Jenis Kelamin</a></li>
+								<li class="nav-item active"> <a href="<?= base_url(); ?>chart-dtks" class="nav-link active">berdasarkan Jenis Kelamin</a></li>
 								<!-- <li class="nav-item"> <a href="<?= base_url(); ?>chart-dtks-jenkel" target="_blank" class="nav-link">berdasarkan Umur</a></li> -->
-								<li class="nav-item"> <a href="<?= base_url(); ?>chart-dtks-kecamatan" target="_blank" class="nav-link">berdasarkan Jenis Kelamin per Kecamatan</a></li>
-								<li class="nav-item"> <a href="<?= base_url(); ?>chart-dtks-kelurahan" target="_blank" class="nav-link">berdasarkan Jenis Kelamin per Kelurahan</a></li>
+								<li class="nav-item"> <a href="<?= base_url(); ?>chart-dtks-kecamatan" class="nav-link">berdasarkan Jenis Kelamin per Kecamatan</a></li>
+								<li class="nav-item"> <a href="<?= base_url(); ?>chart-dtks-kelurahan" class="nav-link">berdasarkan Jenis Kelamin per Kelurahan</a></li>
 
 								<!-- <li class="nav-item"> <button class="nav-link active" id="jenkel-tab" data-bs-toggle="tab" data-bs-target="#jenkel" type="button"aria-selected="true">berdasarkan Jenis Kelamin</button></li>
 								<li class="nav-item"> <button class="nav-link" id="umur-tab" data-bs-toggle="tab" data-bs-target="#umur" type="button"aria-selected="false">berdasarkan Umur</button></li>
