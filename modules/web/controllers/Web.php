c2e8641ef116542d14a92d70c5362c2a57765e09
Akang Adnan <akangadnan@gmail.com>
2023-08-04 07:56:42 +0700

    Ordering Slider

--- modules/web/controllers/Web.php
+++ modules/web/controllers/Web.php
@@ -19,7 +19,7 @@ class Web extends Front {
 	}
 
 	public function index() {
-		$data['sliders']        = $this->db->where('slider_status', '1')->get('sliders')->result();
+		$data['sliders']        = $this->db->where('slider_status', '1')->order_by('slider_id', 'DESC')->get('sliders')->result();
 		$data['categories']     = $this->db->get('blog_category')->result();
 		$data['links']          = $this->db->where('menu_type_id = 3')->order_by('sort', 'ASC')->get('menu')->result();
 		$data['navigation']     = $this->db->where('menu_type_id = 2')->get('menu')->result();
