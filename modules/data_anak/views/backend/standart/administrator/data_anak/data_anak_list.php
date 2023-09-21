c50bdb1aaaea9da517db9a8ba877a29a37e3c45a
Akang Adnan <akangadnan@gmail.com>
2023-09-21 11:20:44 +0700

    data_anak_list

--- modules/data_anak/views/backend/standart/administrator/data_anak/data_anak_list.php
+++ modules/data_anak/views/backend/standart/administrator/data_anak/data_anak_list.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="<?= BASE_ASSET.'datatables/datatables.min.css'; ?>" />
+<link rel="stylesheet" href="<?= BASE_ASSET . 'datatables/datatables.min.css'; ?>" />
 
 <section class="content-header">
 	<h1>
@@ -19,14 +19,14 @@
 						<div class="box box-widget widget-user-2">
 							<div class="widget-user-header">
 								<div class="row pull-right">
-									<?php is_allowed('data_anak_add', function(){?>
-									<a class="btn btn-flat btn-success btn_add_new" id="btn_add_new" title="<?= cclang('add_new_button', [cclang('data_anak')]); ?>  (Ctrl+a)" href="<?=  site_url('administrator/data_anak/add'); ?>">
-										<i class="fa fa-plus-square-o"></i>
-										<?= cclang('add_new_button', [cclang('data_anak')]); ?></a>
+									<?php is_allowed('data_anak_add', function () { ?>
+										<a class="btn btn-flat btn-success btn_add_new" id="btn_add_new" title="<?= cclang('add_new_button', [cclang('data_anak')]); ?>  (Ctrl+a)" href="<?= site_url('administrator/data_anak/add'); ?>">
+											<i class="fa fa-plus-square-o"></i>
+											<?= cclang('add_new_button', [cclang('data_anak')]); ?></a>
 									<?php }) ?>
-									<?php is_allowed('data_anak_export', function(){?>
-									<a class="btn btn-flat btn-success" title="<?= cclang('export'); ?> <?= cclang('data_anak') ?> " href="<?= site_url('administrator/data_anak/export?q='.$this->input->get('q').'&f='.$this->input->get('f')); ?>">
-										<i class="fa fa-file-excel-o"></i> <?= cclang('export'); ?> XLS</a>
+									<?php is_allowed('data_anak_export', function () { ?>
+										<a class="btn btn-flat btn-success" title="<?= cclang('export'); ?> <?= cclang('data_anak') ?> " href="<?= site_url('administrator/data_anak/export?q=' . $this->input->get('q') . '&f=' . $this->input->get('f')); ?>">
+											<i class="fa fa-file-excel-o"></i> <?= cclang('export'); ?> XLS</a>
 									<?php }) ?>
 								</div>
 								<div class="widget-user-image">
@@ -57,58 +57,58 @@
 										</tr>
 									</thead>
 									<tbody>
-<?php
-	if ($data_anak['success'] == true) {
-		$no = 1;
-		foreach ($data_anak['data'] as $item) {
-			if ($item['jenis_kelamin'] == 'L') {
-				$jenkel = 'LAKI-LAKI';
-			}else if ($item['jenis_kelamin'] == 'P') {
-				$jenkel = 'PEREMPUAN';
-			}else{
-				$jenkel = '-';
-			}
-?>
-	<tr>
-		<td><?= $no++;?></td>
-		<td>-</td>
-		<td><?= $item['kecamatan_ktp'];?></td>
-		<td><?= $item['kelurahan_ktp'];?></td>
-		<td><?= $item['no_kk'];?></td>
-		<td><?= $item['nik_anak'];?></td>
-		<td><?= $item['nama_anak'];?></td>
-		<td><?= $jenkel;?></td>
-		<td><?= convertsystemTanggalIndo($item['tanggal_lahir']);?></td>
-		<td><?= $item['usia'];?></td>
-		<td><?= $item['alamat_ktp'];?></td>
-		<td><?= $item['nama_ibu'];?></td>
-		<td width="200">
-			<?php is_allowed('data_anak_profile', function() use ($item){?>
-				<a href="<?= site_url('administrator/data_anak/profile_anak?nik=' . $item['nik_anak']); ?>" class="btn btn-sm btn-default"><i class="fa fa-file-text-o"></i>
-					<?= cclang('profile_anak'); ?></a><br/>
-			<?php }) ?>
-			<?php is_allowed('data_anak_stunting', function() use ($item){?>
-				<a href="<?= site_url('administrator/data_stunting_anak/view_stunting?nik=' . $item['nik_anak']); ?>" class="btn btn-sm btn-danger"><i class="fa fa-file-text-o"></i>
-					<?= cclang('stunting_anak'); ?></a><br/>
-			<?php }) ?>
-			<?php is_allowed('data_perkembangan_anak', function() use ($item){?>
-				<a href="<?= site_url('administrator/perkembangan_anak/view_perkembangan?nik=' . $item['nik_anak']); ?>" class="btn btn-sm btn-info"><i class="fa fa-file-text-o"></i>
-					<?= cclang('perkembangan_anak'); ?></a><br/>
-			<?php }) ?>
-			<?php is_allowed('data_intervensi_anak', function() use ($item){?>
-				<a href="<?= site_url('administrator/data_intervensi_anak/view_intervensi?nik=' . $item['nik_anak']); ?>" class="btn btn-sm btn-primary"><i class="fa fa-file-text-o"></i>
-					<?= cclang('intervensi_anak'); ?></a><br/>
-			<?php }) ?>
-			<?php is_allowed('data_anak_view', function() use ($item){?>
-				<a href="<?= site_url('administrator/data_anak/view/' . $item['nik_anak']); ?>" class="btn btn-sm btn-warning"><i class="fa fa-newspaper-o"></i>
-					<?= cclang('view_button'); ?></a><br/>
-			<?php }) ?>
-		</td>
-	</tr>
-<?php
-		}
-	}
-?>
+										<?php
+										if ($data_anak['success'] == true) {
+											$no = 1;
+											foreach ($data_anak['data'] as $item) {
+												if ($item['jenis_kelamin'] == 'L') {
+													$jenkel = 'LAKI-LAKI';
+												} else if ($item['jenis_kelamin'] == 'P') {
+													$jenkel = 'PEREMPUAN';
+												} else {
+													$jenkel = '-';
+												}
+										?>
+												<tr>
+													<td><?= $no++; ?></td>
+													<td>-</td>
+													<td><?= $item['kecamatan_ktp']; ?></td>
+													<td><?= $item['kelurahan_ktp']; ?></td>
+													<td><?= $item['no_kk']; ?></td>
+													<td><?= $item['nik_anak']; ?></td>
+													<td><?= $item['nama_anak']; ?></td>
+													<td><?= $jenkel; ?></td>
+													<td><?= convertsystemTanggalIndo($item['tanggal_lahir']); ?></td>
+													<td><?= $item['usia']; ?></td>
+													<td><?= $item['alamat_ktp']; ?></td>
+													<td><?= $item['nama_ibu']; ?></td>
+													<td width="200">
+														<?php is_allowed('data_anak_profile', function () use ($item) { ?>
+															<a href="<?= site_url('administrator/data_anak/profile_anak?nik=' . $item['nik_anak']); ?>" class="btn btn-sm btn-default"><i class="fa fa-file-text-o"></i>
+																<?= cclang('profile_anak'); ?></a><br />
+														<?php }) ?>
+														<?php is_allowed('data_anak_stunting', function () use ($item) { ?>
+															<a href="<?= site_url('administrator/data_stunting_anak/view_stunting?nik=' . $item['nik_anak']); ?>" class="btn btn-sm btn-danger"><i class="fa fa-file-text-o"></i>
+																<?= cclang('stunting_anak'); ?></a><br />
+														<?php }) ?>
+														<?php is_allowed('data_perkembangan_anak', function () use ($item) { ?>
+															<a href="<?= site_url('administrator/perkembangan_anak/view_perkembangan?nik=' . $item['nik_anak']); ?>" class="btn btn-sm btn-info"><i class="fa fa-file-text-o"></i>
+																<?= cclang('perkembangan_anak'); ?></a><br />
+														<?php }) ?>
+														<?php is_allowed('data_intervensi_anak', function () use ($item) { ?>
+															<a href="<?= site_url('administrator/data_intervensi_anak/view_intervensi?nik=' . $item['nik_anak']); ?>" class="btn btn-sm btn-primary"><i class="fa fa-file-text-o"></i>
+																<?= cclang('intervensi_anak'); ?></a><br />
+														<?php }) ?>
+														<?php is_allowed('data_anak_view', function () use ($item) { ?>
+															<a href="<?= site_url('administrator/data_anak/view/' . $item['nik_anak']); ?>" class="btn btn-sm btn-warning"><i class="fa fa-newspaper-o"></i>
+																<?= cclang('view_button'); ?></a><br />
+														<?php }) ?>
+													</td>
+												</tr>
+										<?php
+											}
+										}
+										?>
 									</tbody>
 								</table>
 							</div>
@@ -120,14 +120,14 @@
 	</div>
 </section>
 
-<script type="text/javascript" src="<?= BASE_ASSET.'datatables/datatables.min.js'; ?>"></script>
+<script type="text/javascript" src="<?= BASE_ASSET . 'datatables/datatables.min.js'; ?>"></script>
 
 <script type="text/javascript">
-	$(document).ready(function () {
+	$(document).ready(function() {
 		var table = $('.dataTable').DataTable();
 
 		// $.ajax({
-		// 	url: "<?= BASE_URL.'administrator/data_anak/data_anak_stunting';?>",
+		// 	url: "<?= BASE_URL . 'administrator/data_anak/data_anak_stunting'; ?>",
 		// 	dataType: "json",
 		// 	success: function(response) {
 		// 		if (response.success === true) {
